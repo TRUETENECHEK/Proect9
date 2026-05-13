3866aeb424acaf8a88f0d5bb6906d9a52c63995d
skibidi <andrei_kuznetsov007@mail.ru>
2026-05-13 03:25:18 +0300

    перед правками

--- main_subsample.py
+++ main_subsample.py
@@ -7,7 +7,7 @@ SAMPLING_RATE = 1000
 def main() -> None:
     run_pipeline(
         config_path="config.yaml",
-        fastq_path="Read_file/test_reads.fastq",
+        fastq_path="Reserv/test_reads.fastq",
         output_csv="results_subsampled.csv",
         output_dir="report_plots_subsampled",
         sampling_rate=SAMPLING_RATE,
